b5991d96fa69dc17fade9464fb3d9cca9e3ebb0f
Maira <mairaesper24@gmail.com>
2025-03-26 16:38:29 -0300

    Clases

---
+++ src/EmpleadoDAO.java
@@ -0,0 +1,22 @@
+public class EmpleadoDAO implements IEmpleadoDAO{
+
+    @Override
+    public void agregarEmpleado(Empleado empleado) {
+        
+    }
+
+    @Override
+    public Empleado obtenerEmpleado(String nombre) {
+        return null;
+    }
+
+    @Override
+    public void eliminarEmpleado(String nombre) {
+
+    }
+
+    @Override
+    public void actualizarEmpleado(Empleado empleado) {
+
+    }
+}
